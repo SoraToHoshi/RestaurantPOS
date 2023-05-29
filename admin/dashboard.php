f37d7771f0796d76e45db55110dfb4160ceab30a
SoraToHoshi <73216747+SoraToHoshi@users.noreply.github.com>
2023-05-29 15:44:59 +0800

    Food Republic Gen Tri POS
    A Capstone Project

---
+++ admin/dashboard.php
@@ -0,0 +1,145 @@
+<?php
+session_start();
+include('config/config.php');
+include('config/checklogin.php');
+check_login();
+require_once('partials/_head.php');
+require_once('partials/_analytics.php');
+?>
+
+<body>
+  <?php
+  require_once('partials/_sidebar.php');
+  ?>
+  
+  <div class="main-content">
+    <?php
+    require_once('partials/_topnav.php');
+    ?>
+    
+    <div style="background-image: url(assets/img/theme/restro00.jpg); background-size: cover;" class="header  pb-8 pt-5 pt-md-8">
+      <span class="mask bg-gradient-dark opacity-8"></span>
+      <div class="container-fluid">
+        <div class="header-body">
+          
+          <div class="row">
+            <div class="col-xl-3 col-lg-6">
+              <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Customers</h5>
+                      <span class="h2 font-weight-bold mb-0"><?php echo $customers; ?></span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                        <i class="fas fa-users"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+			
+            <div class="col-xl-3 col-lg-6">
+              <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Products</h5>
+                      <span class="h2 font-weight-bold mb-0"><?php echo $products; ?></span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-primary text-white rounded-circle shadow">
+                        <i class="fas fa-utensils"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6">
+              <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Orders</h5>
+                      <span class="h2 font-weight-bold mb-0"><?php echo $orders; ?></span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
+                        <i class="fas fa-shopping-cart"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6">
+              <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
+                      <span class="h2 font-weight-bold mb-0">$<?php echo $sales; ?></span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-green text-white rounded-circle shadow">
+                        <i class="fas fa-dollar-sign"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+   
+                <tbody>
+                 
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+		
+     
+              
+              <table class="table align-items-center table-flush">
+                <thead class="thead-light">
+                  <tr>
+                   
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  $ret = "SELECT * FROM   rpos_payments   ORDER BY `rpos_payments`.`created_at` DESC LIMIT 7 ";
+                  $stmt = $mysqli->prepare($ret);
+                  $stmt->execute();
+                  $res = $stmt->get_result();
+                  while ($payment = $res->fetch_object()) {
+                  ?>
+                    <tr>
+                     
+                    </tr>
+                  <?php } ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php require_once('partials/_footer.php'); ?>
+    </div>
+  </div>
+
+  <?php
+  require_once('partials/_scripts.php');
+  ?>
+</body>
+
+</html>
